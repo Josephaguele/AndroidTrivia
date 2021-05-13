07d1cc642aafc490a3e81b37d67970bc15027698
Joseph <osekuemen@gmail.com>
2021-05-13 14:16:37 +0100

    Step 8 - Sharing Success Intent

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,16 +16,14 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
-
 
 class GameWonFragment : Fragment() {
 
@@ -40,9 +38,52 @@ class GameWonFragment : Fragment() {
             view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
 
-        // Display the arguments using a Toast
-        val args = GameWonFragmentArgs.fromBundle(requireArguments())
-        Toast.makeText(context,"NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+        // Declaring that our fragment has a Menu
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    // Creating our Share Intent
+    private fun getShareIntent(): Intent {
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text,args.numQuestions, args.numQuestions))
+        return shareIntent
+    }
+
+    // Starting an Activity with our new Intent
+    private fun shareSuccess()
+    {
+        startActivity(getShareIntent())
+    }
+
+    /*He we override onCreateOptionsmMenu and begin by inflating the winner_menu. Then we'll get the shareIntent
+    * using getShareIntent() and call resolveActivity using the packageManager to make sure our shareIntent
+    * resolves to an activity
+    * If the result equals null, which menas that it doesn't resolve, we find our sharing menu item from the
+    * inflated menu and set its visibility to false*/
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+        // check if the activity resolves
+        if (null == getShareIntent().resolveActivity(requireActivity().packageManager)){
+            // hide the menu item if it doesn't resolve
+            menu.findItem(R.id.share)?.isVisible = false
+        }
+    }
+
+    //TODO -- There is an important note below
+    // NOTE This means that if there is an available destination for our shareIntent, the icon will
+    // appear and we can share. Otherwise, it will not be available
+
+    // Sharing the menu
+    /* When the menuitem id matches R.id.share, call the shareSuccess method*/
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
+
